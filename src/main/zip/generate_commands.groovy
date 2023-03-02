1fe015d3627828a3c32167544d178cf0c423600e
Shabaz basha Kowthalam <shabazbasha.kowth@hcl.com>
2023-03-02 12:12:41 +0530

    Updated license info

--- src/main/zip/generate_commands.groovy
+++ src/main/zip/generate_commands.groovy
@@ -1,5 +1,5 @@
 /**
- * © Copyright IBM Corporation 2016, 2018.
+ * © Copyright IBM Corporation 2016, 2023.
  * This is licensed under the following license.
  * The Eclipse Public 1.0 License (http://www.eclipse.org/legal/epl-v10.html)
  * U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
